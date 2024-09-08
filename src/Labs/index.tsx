1368af0bf6a8dcbda5a1569eab62bfe642afe3f6
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-09-08 10:48:24 -0400

    a1 lab

---
+++ src/Labs/index.tsx
@@ -0,0 +1,14 @@
+import Lab1 from "./Lab1";
+import Lab2 from "./Lab2";
+
+export default function Labs(){
+
+    return (
+        <div>
+    <h1>Labs</h1>
+    <Lab1/>
+    <Lab2/>
+    <h4>Lab 3</h4>
+        </div>
+    );
+}

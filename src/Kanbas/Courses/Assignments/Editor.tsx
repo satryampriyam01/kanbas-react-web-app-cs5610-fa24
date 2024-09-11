c2d6f6079f66c6048afc4ee4931cf4ca0a43c205
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-09-11 15:23:38 -0400

    Editor kanbas

---
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -0,0 +1,101 @@
+export default function AssignmentEditor() {
+    return (
+      <div id="wd-assignments-editor">
+        <label htmlFor="wd-name">Assignment Name</label>
+        <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
+        <textarea id="wd-description">
+          The assignment is available online Submit a link to the landing page of
+        </textarea>
+        <br />
+        <table>
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-points">Points</label>
+          </td>
+          <td>
+            <input id="wd-points" value={100} />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label htmlFor="wd-group"> Assignment Group </label>
+            <select id="wd-group">
+              <option value="ASSINMENTS"> ASSIGNMENTS</option>
+              <option value="OTHERS"> OTHERS</option>
+            </select>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+          <label htmlFor="wd-display-grade-as"> Display Grade as </label>
+          <select id="wd-display-grade-as">
+          <option value="PERCENTAGE">Percentage</option>
+          <option value="OTHERS">Others</option>
+          </select>
+          </td>
+        </tr>
+
+
+        <tr>
+          <td>
+          <label htmlFor="wd-submission-type"> Submission Type </label>
+          <select id="wd-submission-type">
+          <option value="ONLINE">Online</option>
+          <option value="OTHERS">Others</option>
+          </select>
+<br/>
+          <label>Online Entry option</label><br/>
+            <input type="checkbox" name="check-submission" id="wd-text-entry"/>
+<label htmlFor="wd-text-entry">Text Entry</label><br/>
+
+<input type="checkbox" name="check-submission" id="wd-website-url"/>
+<label htmlFor="wd-website-url">Website URL</label><br/>
+
+<input type="checkbox" name="check-submission" id="wd-media-recordings"/>
+<label htmlFor="wd-media-recordings">Media Recordings</label><br/>
+
+<input type="checkbox" name="check-submission" id="wd-student-annotation"/>
+<label htmlFor="wd-student-annotation">Student Annotation</label><br/>
+
+
+<input type="checkbox" name="check-submission" id="wd-file-upload"/>
+<label htmlFor="wd-file-upload">File Upload</label><br/>
+
+          </td>
+        </tr>
+
+        <tr>
+
+          <td>
+           Assign 
+      <label htmlFor="wd-assign-to">Assign to</label>
+      <br/>
+
+      <label htmlFor="wd-due-date">Due </label>
+      <input type="date" id="wd-due-date"></input>
+      <br/>
+
+      <label htmlFor="wd-available-from">From </label>
+      <input type="date" id="wd-available-from"></input>
+      
+      <label htmlFor="wd-available-until">Untill </label>
+      <input type="date" id="wd-available-until"></input>
+      
+
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+
+          </td>
+        </tr>
+        <div id="wd-name" >
+        <button type="button">Cancel</button>
+        <button type="button">Save</button>
+        </div>
+
+      </table>
+    </div>
+);}

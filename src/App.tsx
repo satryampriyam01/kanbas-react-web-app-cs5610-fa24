f3eba2ca77c7129d193623e444ec3e1ce88193ed
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-09-17 20:15:45 -0400

    Final production for A1 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
-import Labs from "./Labs"
-import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Kanbas from './Kanbas';
-import SourceCode from './SourceCode';
+import React from "react";
+import "./App.css";
+import Labs from "./Labs";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import Kanbas from "./Kanbas";
+import SourceCode from "./SourceCode";
 
 function App() {
   return (
@@ -13,12 +13,10 @@ function App() {
           <Route path="/" element={<Navigate to="Labs" />} />
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
-          <Route path='/Sourcecode/*' element={<SourceCode/>}/>
+          <Route path="/Sourcecode/*" element={<SourceCode />} />
         </Routes>
       </div>
     </HashRouter>
-
-    
   );
 }
 
--- src/SourceCode/index.tsx
+++ src/SourceCode/index.tsx
@@ -1,10 +1,11 @@
-
-
-
 export default function SourceCode() {
-
-    return(
-        <a href="https://github.com/satryampriyam01/kanbas-react-web-app-cs5610-fa24" id="wd-github-link" target="_blank">
-          Satyam Priyam Github
-        </a>);
+  return (
+    <a
+      href="https://github.com/satryampriyam01/kanbas-react-web-app-cs5610-fa24"
+      id="wd-github-link"
+      target="_blank"
+    >
+      Satyam Priyam Github
+    </a>
+  );
 }

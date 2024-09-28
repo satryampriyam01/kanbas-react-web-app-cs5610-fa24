6939bbef1129a9a241690a7a6b6f9d77ed37f893
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-09-28 14:10:36 -0400

    Lab-work work 2 pre prod-1

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -32,7 +32,8 @@ export default function Assignments() {
 
       <ul id="wd-assignment-list" className="list-group rounded-0">
         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary d-flex align-items-center justify-content-between">
+          <div className="wd-title p-3 ps-2 bg-secondary d-flex justify-content-between align-items-center">
+            {/* Title Section */}
             <div className="d-flex align-items-center">
               <BsGripVertical className="me-2 fs-3" />
               <h3 id="wd-assignments-title" className="mb-0 text-white">
@@ -40,11 +41,12 @@ export default function Assignments() {
               </h3>
             </div>
 
+            {/* Right-side Section with 40%, Plus Icon, and Lesson Control */}
             <div className="d-flex align-items-center">
               <span className="badge bg-light text-dark rounded-3 me-2">
                 40% of Total
               </span>
-              <BsPlus className="me-2" />
+              <BsPlus className="me-3" />
               <LessonControlButtons />
             </div>
           </div>
@@ -52,11 +54,13 @@ export default function Assignments() {
           {/* Assignment List */}
           <ul className="wd-lessons list-group rounded-0 position-relative">
             <div className="border-start border-3 border-success position-absolute start-0 h-100" />
+
             {/* Assignment A1 */}
             <li
               className="wd-lesson list-group-item p-3 ps-1"
               id="wd-assignment-a1"
             >
+              <div className="d-flex justify-content-between align-items-center">
               <div className="d-flex align-items-center ms-3">
                 <BsBook className="text-success me-3 fs-4" />
                 <div>
@@ -76,8 +80,9 @@ export default function Assignments() {
                     <b>Due</b> May 13 at 11:59pm | 100 points
                   </small>
                 </div>
+                </div>
+                <LessonControlButtons />
               </div>
-              <LessonControlButtons />
             </li>
 
             {/* Assignment A2 */}
@@ -85,27 +90,29 @@ export default function Assignments() {
               className="wd-lesson list-group-item p-3 ps-1 mt-3"
               id="wd-assignment-a2"
             >
-              <div className="d-flex align-items-center ms-3">
-                <BsBook className="text-success me-3 fs-4" />
-                <div>
-                  <a
-                    className="wd-assignment-link h5 mb-1"
-                    href="#/Kanbas/Courses/1234/Assignments/124"
-                  >
-                    A2 - CSS + BOOTSTRAP
-                  </a>
-                  <br />
-                  <small>
-                    Multiple Modules | <b>NOT available until</b> May 13 at
-                    12:00am
-                  </small>
-                  <br />
-                  <small>
-                    <b>Due</b> May 20 at 11:59pm | 100 points
-                  </small>
+              <div className="d-flex justify-content-between align-items-center">
+                <div className="d-flex align-items-center ms-3">
+                  <BsBook className="text-success me-3 fs-4" />
+                  <div>
+                    <a
+                      className="wd-assignment-link h5 mb-1"
+                      href="#/Kanbas/Courses/1234/Assignments/124"
+                    >
+                      A2 - CSS + BOOTSTRAP
+                    </a>
+                    <br />
+                    <small>
+                      Multiple Modules | <b>NOT available until</b> May 13 at
+                      12:00am
+                    </small>
+                    <br />
+                    <small>
+                      <b>Due</b> May 20 at 11:59pm | 100 points
+                    </small>
+                  </div>
                 </div>
+                <LessonControlButtons />
               </div>
-              <LessonControlButtons />
             </li>
 
             {/* Assignment A3 */}
@@ -113,27 +120,32 @@ export default function Assignments() {
               className="wd-lesson list-group-item p-3 ps-1 mt-3"
               id="wd-assignment-a3"
             >
-              <div className="d-flex align-items-center ms-3">
-                <BsBook className="text-success me-3 fs-4" />
-                <div>
-                  <a
-                    className="wd-assignment-link h5 mb-1"
-                    href="#/Kanbas/Courses/1234/Assignments/125"
-                  >
-                    A3 - JAVASCRIPT + REACT
-                  </a>
-                  <br />
-                  <small>
-                    Multiple Modules | <b>NOT available until</b> May 20 at
-                    12:00am
-                  </small>
-                  <br />
-                  <small>
-                    <b>Due</b> May 27 at 11:59pm | 100 points
-                  </small>
+              <div className="d-flex justify-content-between align-items-center">
+                {/* Assignment details on the left */}
+                <div className="d-flex align-items-center ms-3">
+                  <BsBook className="text-success me-3 fs-4" />
+                  <div>
+                    <a
+                      className="wd-assignment-link h5 mb-1"
+                      href="#/Kanbas/Courses/1234/Assignments/125"
+                    >
+                      A3 - JAVASCRIPT + REACT
+                    </a>
+                    <br />
+                    <small>
+                      Multiple Modules | <b>NOT available until</b> May 20 at
+                      12:00am
+                    </small>
+                    <br />
+                    <small>
+                      <b>Due</b> May 27 at 11:59pm | 100 points
+                    </small>
+                  </div>
                 </div>
+
+                {/* LessonControlButtons aligned on the right */}
+                <LessonControlButtons />
               </div>
-              <LessonControlButtons />
             </li>
           </ul>
         </li>

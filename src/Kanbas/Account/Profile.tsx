9597a92f09ea027487dbac27675b5200ca7b5bb6
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-11-12 10:58:07 -0500

    Kanbas Update

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -2,12 +2,17 @@ import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
 import { Link, useNavigate } from "react-router-dom";
-
+import * as client from "./client";
 export default function Profile() {
   const [profile, setProfile] = useState<any>({});
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const updateProfile = async () => {
+    const updatedProfile = await client.updateUser(profile);
+    dispatch(setCurrentUser(updatedProfile));
+  };
+
   const fetchProfile = () => {
     if (!currentUser) return navigate("/Kanbas/Account/Signin");
     setProfile(currentUser);
@@ -80,6 +85,7 @@ export default function Profile() {
             <option value="FACULTY">Faculty</option>
             <option value="STUDENT">Student</option>
           </select>
+          <button onClick={updateProfile} className="btn btn-primary w-100 mb-2"> Update </button>
           <button
             onClick={signout}
             className="btn btn-danger w-100 mb-2"
--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -6,3 +6,15 @@ export const signin = async (credentials: any) => {
   const response = await axios.post( `${USERS_API}/signin`, credentials );
   return response.data;
 };
+
+export const signup = async (user: any) => {
+    const response = await axios.post(`${USERS_API}/signup`, user);
+    return response.data;
+  };
+  
+
+  export const updateUser = async (user: any) => {
+    const response = await axios.put(`${USERS_API}/${user._id}`, user);
+    return response.data;
+  };
+  

772067b58ab38f384f712d90345c72630d7c0b4f
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-11-15 12:46:21 -0500

    Pre Production hotfix-3 image default

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -72,7 +72,12 @@ export default function Dashboard({ courses, course, setCourse, addNewCourse,
                             <div className="card rounded-3 overflow-hidden">
                                 <Link to={`/Kanbas/Courses/${course._id}/Home`}
                                     className="wd-dashboard-course-link text-decoration-none text-dark" >
-                                    <img src={`/images/${course._id}.jpg`} width="100%" height={160} />
+                                    <img src={`/images/${course._id}.jpg`} width="100%" height={160}
+                                    onError={(e) => {
+                                      e.currentTarget.src = "/images/react.jpg"; // Default fallback image
+                                      e.currentTarget.onerror = null; // Prevent infinite loop if fallback fails
+                                    }}
+                                    />
                                     <div className="card-body">
                                         <h5 className="wd-dashboard-course-title card-title">
                                             {course.name} </h5>

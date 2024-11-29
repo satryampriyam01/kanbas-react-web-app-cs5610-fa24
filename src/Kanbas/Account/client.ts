b47980eafbe6e941490cd65cf8f7b5fb499211b1
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-11-29 11:47:45 -0500

    Dev -2

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -74,3 +74,14 @@ export const findUsersByRole = async (role: string) => {
   return response.data;
 };
 
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
+
+
+export const findUserById = async (id: string) => {
+  const response = await axios.get(`${USERS_API}/${id}`);
+  console.log(id);
+  return response.data;
+};
--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { useParams } from "react-router-dom";
-import * as db from "../../Database";
+import { Link, useParams } from "react-router-dom";
+import PeopleDetails from "./Details";
 
 export default function PeopleTable({ users = [] }: { users?: any[] }) {
   return (
     <div id="wd-people-table">
+       <PeopleDetails />
       <table className="table table-striped">
         <thead>
           <tr>
@@ -18,13 +19,19 @@ export default function PeopleTable({ users = [] }: { users?: any[] }) {
           </tr>
         </thead>
         <tbody>
+          
           {users
             .map((user: any) => (
+              
               <tr key={user._id}>
                 <td className="wd-full-name text-nowrap">
+                
+                <Link to={`/Kanbas/Account/Users/${user._id}`} className="text-decoration-none">
+
                   <FaUserCircle className="me-2 fs-1 text-secondary" />
                   <span className="wd-first-name">{user.firstName}</span>
                   <span className="wd-last-name">{user.lastName}</span>
+                  </Link>
                 </td>
                 <td className="wd-login-id">{user.loginId}</td>
                 <td className="wd-section">{user.section}</td>
@@ -35,6 +42,7 @@ export default function PeopleTable({ users = [] }: { users?: any[] }) {
             ))}
         </tbody>
       </table>
+
     </div>
   );
 }

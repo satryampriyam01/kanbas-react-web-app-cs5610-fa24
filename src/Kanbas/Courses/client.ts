574aaa5f45e5cd19af218d65d50c163899b54fc6
Satyam Priyam <53575651+satryampriyam01@users.noreply.github.com>
2024-12-02 22:10:47 -0500

    Dev A6-5.3

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -13,11 +13,11 @@ export const deleteCourse = async (id: string) => {
   return data;
 };
 export const updateCourse = async (course: any) => {
-  const { data } = await axios.put(`${COURSES_API}/${course._id}`, course);
+  const { data } = await axiosWithCredentials.put(`${COURSES_API}/${course._id}`, course);
   return data;
 };
 export const findModulesForCourse = async (courseId: string) => {
-  const response = await axios.get(`${COURSES_API}/${courseId}/modules`);
+  const response = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/modules`);
   return response.data;
 };
 export const createModuleForCourse = async (courseId: string, module: any) => {
